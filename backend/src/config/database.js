76d55ad09e79a0ec3eb8ea99f475055b2e10c61a
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-03-10 13:16:17 -0300

    Preparação para configurações adicionais sequelize

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'root',
+  port: '5432',
+  database: 'eventmanager',
+  define: {
+    timestamps: true,
+  }
+}
